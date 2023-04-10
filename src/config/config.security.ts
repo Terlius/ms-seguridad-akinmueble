4492107c2b886b88b1ba47a6c9b01daae1be7c05
Miguel Mateo Grajales <matius404@gmail.com>
2023-04-09 22:43:05 -0500

    Se modifica el archivo config con una nueva variable de entorno para keyJWT

--- src/config/config.security.ts
+++ src/config/config.security.ts
@@ -1,5 +1,5 @@
 export namespace securityConfig {
-  export const keyJWT =  "Admin@2023";
+  export const keyJWT =  process.env.SECRET_PASS_JWT;
   export const menuUserId = "64276c1a7ce597744892de1b";
   export const listAction = "list";
   export const saveAction = "save";
